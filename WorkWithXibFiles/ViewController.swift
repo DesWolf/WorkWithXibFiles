91a27d4738522ae0834b5670bf97ef8e5be40ba4
DesWolf <okuneev@gmail.com>
2020-02-17 16:05:39 +0700

    Initial Commit

---
+++ WorkWithXibFiles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkWithXibFiles
+//
+//  Created by Максим Окунеев on 2/17/20.
+//  Copyright © 2020 Максим Окунеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
